62721c87804992b5bafd3ba86c2fce8d11a98869
ivk0vic <ivk0vic@hotmail.com>
2023-08-03 16:21:40 +0200

    Dodata pocetna hijerarhija komponenti

---
+++ client/src/components/common/FooterMobile.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class FooterMobile extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default FooterMobile;
---
+++ client/src/components/common/NavMenuDesktop.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class NavMenuDesktop extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default NavMenuDesktop;
