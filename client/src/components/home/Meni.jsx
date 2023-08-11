45cf701620090768ce2100b3e4d9ac3cfdc8ddb2
MilicaLjubojevic <milica.ljubojevic98@gmail.com>
2023-08-11 12:18:03 +0200

    Dodavanje Kategorija i Podkategorija na sajt

--- client/src/components/home/Meni.jsx
+++ client/src/components/home/Meni.jsx
@@ -1,238 +1,51 @@
 import React, { Component, Fragment } from "react";
 
-import c1 from "../../assets/images/c1.png";
-
 class Meni extends Component {
-    constructor() {
+    constructor(props) {
         super();
-        this.MegaMenu = this.MegaMenu.bind(this);
     }
 
-    componentDidMount() {
-        this.MegaMenu();
-    }
-
-    MegaMenu() {
-        var acc = document.getElementsByClassName("accordion");
-        var accNum = acc.length;
-        var i;
-        for (i = 0; i < accNum; i++) {
-            acc[i].addEventListener("click", function () {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                }
-            });
+    MenuItemClick = (event) => {
+        event.target.classList.toggle("active");
+        var panel = event.target.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
         }
-    }
+    };
 
     render() {
-        return (
-            <div className="accordionMenuDiv">
-                <div className="accordionMenuDivInside">
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 1
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+        const CatList = this.props.data;
 
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 2
+        const MyView = CatList.map((CatList, i) => {
+            return (
+                <div key={i.toString()}>
+                    <button onClick={this.MenuItemClick} className="accordion">
+                        &nbsp;
+                        {CatList.category_name}
                     </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
 
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 3
-                    </button>
                     <div className="panel">
                         <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 4
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 5
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 6
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 7
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <button className="accordion">
-                        <img
-                            className="accordionMenuIcon"
-                            src={c1}
-                            width="25px"
-                        />
-                        &nbsp; Kategorija 8
-                    </button>
-                    <div className="panel">
-                        <ul>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 1
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="accordionItem">
-                                    {" "}
-                                    Podkategorija 2
-                                </a>
-                            </li>
+                            {CatList.subcategory_name.map((SubList, i) => {
+                                return (
+                                    <li>
+                                        <a href="#" className="accordionItem">
+                                            {SubList.subcategory_name}{" "}
+                                        </a>
+                                    </li>
+                                );
+                            })}
                         </ul>
                     </div>
                 </div>
+            );
+        });
+
+        return (
+            <div className="accordionMenuDiv">
+                <div className="accordionMenuDivInside">{MyView}</div>
             </div>
         );
     }
--- client/src/components/home/Vrh.jsx
+++ client/src/components/home/Vrh.jsx
@@ -3,19 +3,36 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 //import Slajd from "./Slajd";
 import HomeSlajd from "./HomeSlajd";
 import Meni from "./Meni";
+import AppURL from "../../api/AppURL";
+import axios from "axios";
 
 class Vrh extends Component {
+    constructor() {
+        super();
+        this.state = {
+            MenuData: [],
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get(AppURL.AllCategoryDetails)
+            .then((response) => {
+                this.setState({ MenuData: response.data });
+            })
+            .catch((error) => {});
+    }
+
     render() {
         return (
             <Fragment>
                 <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                     <Row>
-                        <Col lg={3} md={3} sm={12}>
-                            <Meni />
-                        </Col>
-
                         <Col lg={9} md={9} sm={12}>
                             <HomeSlajd />
+                        </Col>
+                        <Col lg={3} md={3} sm={12}>
+                            <Meni data={this.state.MenuData} />
                         </Col>
                     </Row>
                 </Container>
