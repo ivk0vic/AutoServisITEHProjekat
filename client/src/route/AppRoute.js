2c62630ea192f671825fb931878ce7e169c47f8e
andjelaaap <aandjelapetrovic0@gmail.com>
2023-08-16 14:46:28 +0200

    postavljanje jedinstvenih ključeva prilikom rutiranja

--- client/src/route/AppRoute.js
+++ client/src/route/AppRoute.js
@@ -1,40 +1,52 @@
 import React, { Component, Fragment } from 'react'
 import { Router, Route, Switch } from "react-router";
-import HomePage from '../pages/HomePage';
-import UserLoginPage from '../pages/UserLoginPage';
-import ContactPage from '../pages/ContactPage';
-import PurchasePage from '../pages/PurchasePage';
-import PrivacyPage from '../pages/PrivacyPage';
-import RefundPage from '../pages/RefundPage';
-import DetaljnijiPrikazPage from '../pages/DetaljnijiPrikazPage';
-import OmiljenoPage from '../pages/OmiljenoPage';
-import NotificationPage from '../pages/NotificationPage';
-import KorpaPage from '../pages/KorpaPage';
 import AboutPage from '../pages/AboutPage';
+import KorpaPage from '../pages/KorpaPage';
+import ContactPage from '../pages/ContactPage';
+import HomePage from '../pages/HomePage';
+import NotificationPage from '../pages/NotificationPage';
+import PrivacyPage from '../pages/PrivacyPage';
 import ProductCategoryPage from '../pages/ProductCategoryPage';
 import ProductSubCategoryPage from '../pages/ProductSubCategoryPage';
+import PurchasePage from '../pages/PurchasePage';
+import RefundPage from '../pages/RefundPage';
+import UserLoginPage from '../pages/UserLoginPage';
+import DetaljnijiPrikazPage from '../pages/DetaljnijiPrikazPage';
+import OmiljenoPage from '../pages/OmiljenoPage';
 
 class AppRoute extends Component {
      render() {
           return (
      <Fragment>
           <Switch>
-               <Route exact path="/" component={HomePage} />
-               <Route exact path="/login" component={UserLoginPage} />
-               <Route exact path="/contact" component={ContactPage} />
+               
 
-               <Route exact path="/productdetails/:code" component={DetaljnijiPrikazPage} />
-               <Route exact path="/notification" component={NotificationPage} />
-               <Route exact path="/favourite" component={OmiljenoPage} />
-               <Route exact path="/cart" component={KorpaPage} />
+ <Route exact path="/" render={(props) => <HomePage {...props} key={Date.now()} /> } />
 
-               <Route exact path="/purchase" component={PurchasePage} />
-               <Route exact path="/privacy" component={PrivacyPage} />
-               <Route exact path="/refund" component={RefundPage} />
-               <Route exact path="/about" component={AboutPage} />    
+ <Route exact path="/login" render={(props) => <UserLoginPage {...props} key={Date.now()} /> } />
+ 
+ <Route exact path="/contact" render={(props) => <ContactPage {...props} key={Date.now()} /> } />
 
-               <Route exact path="/productcategory/:category" component={ProductCategoryPage} />
-               <Route exact path="/productsubcategory/:category/:subcategory" component={ProductSubCategoryPage} /> 
+ <Route exact path="/purchase" render={(props) => <PurchasePage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/privacy" render={(props) => <PrivacyPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/refund" render={(props) => <RefundPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/about" render={(props) => <AboutPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productdetails/:code" render={(props) => <DetaljnijiPrikazPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/notification" render={(props) => <NotificationPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/favourite" render={(props) => <OmiljenoPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/cart" render={(props) => <KorpaPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productcategory/:category" render={(props) => <ProductCategoryPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productsubcategory/:category/:subcategory" render={(props) => <ProductSubCategoryPage {...props} key={Date.now()} /> } /> 
+              
 
           </Switch>
 
